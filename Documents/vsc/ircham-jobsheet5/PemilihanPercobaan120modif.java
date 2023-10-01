05a7ce6ecaee82474233c1d8591b1d1b4f4a6067
Icamdaf <icampoltek@gmail.com>
2023-10-01 14:53:25 +0700

    jobsheet5

---
+++ Documents/vsc/ircham-jobsheet5/PemilihanPercobaan120modif.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan120 {
+    public static void main(String[] args) {
+        Scanner input20 = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka = input20.nextInt();
+
+        String jenisBilangan = (angka % 2 == 0) ? "genap" : "ganjil";
+        System.out.println("Angka " + angka + " adalah bilangan " + jenisBilangan + ".");
+    }
+}
